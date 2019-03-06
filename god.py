491320cb0b3f6524d94c49366b683cd948e3c32b
Simon Josi <me@yokto.net>
2019-03-06 10:06:51 +0100

    add support for multiple intent media files

--- god.py
+++ god.py
@@ -2,32 +2,44 @@ from lib.audio import audio_int, listen_for_speech, save_speech
 from lib.stt import detect_intent_audio
 import time, os, sys
 import subprocess, signal
+import random
 
+INTENT_MEDIA_PATH = '/media'
+INTENT_LOOP = 'loop'
+INTENT_FALLBACK = 'fallback'
 CONFIDENCE_TRESHOLD = 0.6
 MPLAYER=['mplayer', '-fs', '-fixed-vo', '-really-quiet']
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow.json"
 
 audio_int(150)
-if sys.argv[1] == 'test':
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
     sys.exit(0)
 
 def play_clip(filename):
-    cmd = 'mplayer -fs -fixed-vo -really-quiet media/%s.mp4' % filename
+    cmd = 'mplayer -fs -fixed-vo -really-quiet %s' % filename
     print(cmd)
     os.system(cmd)
 
 def play_loop():
     print("play loop")
+    if not get_random_intent_media_path(INTENT_LOOP):
+        print("loop media not found")
+        print("sleeping for 5 seconds")
+        time.sleep(5)
+        return
     return subprocess.Popen(
-        MPLAYER + ['-loop', '0', 'media/loop.mp4'],
-        stdout=subprocess.PIPE, 
+        MPLAYER + ['-loop', '0', get_random_intent_media_path(INTENT_LOOP)],
+        stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         shell=False, preexec_fn=os.setsid)
 
 def kill_player(process):
     print("stop loop")
-    print(process.pid)
-    process.terminate()
+    try:
+        print(process.pid)
+        process.terminate()
+    except:
+        pass
     os.system("killall -9 mplayer")
     time.sleep(1)
 
@@ -35,6 +47,16 @@ def kill_subprocesses_and_exit(*args):
     global loop_player
     os.killpg(loop_player.pid, signal.SIGTERM)
     sys.exit(0)
+
+def get_random_intent_media_path(intent):
+    try:
+        intent_media_files = os.listdir(os.path.join(INTENT_MEDIA_PATH, intent))
+        if len(intent_media_files):
+            return os.path.join(INTENT_MEDIA_PATH, random.choice(intent_media_files))
+        else:
+            return False
+    except OSError:
+        return False
 
 loop_player = play_loop()
 signal.signal(signal.SIGINT, kill_subprocesses_and_exit)
@@ -47,9 +69,9 @@ while True:
     print(response)
     kill_player(loop_player)
     if response['confidence'] > CONFIDENCE_TRESHOLD \
-    or os.path.exists('/media/%s.mp4' % response['intent']):
-        play_clip(response['intent'])
+    and get_random_intent_media_path(response['intent']):
+        play_clip(get_random_intent_media_path(response['intent']))
     else:
-        play_clip('fallback')
+        play_clip(get_random_intent_media_path(INTENT_FALLBACK))
 
     loop_player = play_loop()
